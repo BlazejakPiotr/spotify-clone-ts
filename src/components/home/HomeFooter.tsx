b7bffd4b7bbbddb1cd4615d56f9de638598bacc5
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-01-16 21:34:52 +0100

    footer links

--- src/components/home/HomeFooter.tsx
+++ src/components/home/HomeFooter.tsx
@@ -3,8 +3,8 @@ import { Col, Container, Row } from "react-bootstrap";
 const HomeFooter = () => {
   return (
     <Container>
-      <Row>
-        <Col md={12} lg={2} className="text-start">
+      <Row className="text-start">
+        <Col md={12} lg={2}>
           <img
             src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
             alt="logo"
@@ -12,16 +12,41 @@ const HomeFooter = () => {
           />
         </Col>
         <Col md={4} lg={2}>
-          <h6>section title</h6>
+          <h6>COMPANY</h6>
+
+          <p>About</p>
+          <p>Jobs</p>
+          <p>For the Record</p>
         </Col>
         <Col md={4} lg={2}>
-          <h6>section title</h6>
+          <h6>COMMUNITIES</h6>
+
+          <p>For Artists</p>
+          <p>Developers</p>
+          <p>Advertising</p>
+          <p>Investors</p>
+          <p>Vendors</p>
         </Col>
         <Col md={4} lg={2}>
-          <h6>section title</h6>
+          <h6>USEFUL LINKS</h6>
+
+          <p>Support</p>
+          <p>Web Player</p>
+          <p>Free Mobile App</p>
         </Col>
-        <Col md={4} lg={4}>
-          <h6>ICON ICON ICON</h6>
+        <Col md={4} lg={4} className="text-end">
+          <h6>I T F</h6>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10}>
+          <ul className="bottom-bar">
+            <li>Legal</li>
+            <li>lirivacy Center</li>
+            <li>lirivacy liolicy</li>
+            <li>Cookies</li>
+            <li>About Ads</li>
+          </ul>
         </Col>
       </Row>
     </Container>
