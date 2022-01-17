b024a8aa637ba735e71b21fa917af98fcb6280b0
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-01-17 14:30:22 +0100

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./components/home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      {/* <Login /> */}
     </div>
   );
 }
