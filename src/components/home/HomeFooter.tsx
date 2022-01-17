c4c2b86bb6caf57a4a9f1e68075353b41b5e18bb
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-01-17 12:40:20 +0100

    footer

--- src/components/home/HomeFooter.tsx
+++ src/components/home/HomeFooter.tsx
@@ -1,9 +1,16 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 
 const HomeFooter = () => {
   return (
     <Container>
-      <Row className="text-start">
+      <Row className="text-start mb-5">
         <Col md={12} lg={2}>
           <img
             src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
@@ -14,39 +21,93 @@ const HomeFooter = () => {
         <Col md={4} lg={2}>
           <h6>COMPANY</h6>
 
-          <p>About</p>
-          <p>Jobs</p>
-          <p>For the Record</p>
+          <p>
+            <a href="#">About</a>
+          </p>
+          <p>
+            <a href="#">Jobs</a>
+          </p>
+          <p>
+            <a href="#">For the Record</a>
+          </p>
         </Col>
         <Col md={4} lg={2}>
           <h6>COMMUNITIES</h6>
 
-          <p>For Artists</p>
-          <p>Developers</p>
-          <p>Advertising</p>
-          <p>Investors</p>
-          <p>Vendors</p>
+          <p>
+            <a href="#">For Artists</a>
+          </p>
+          <p>
+            <a href="#">Developers</a>
+          </p>
+          <p>
+            <a href="#">Advertising</a>
+          </p>
+          <p>
+            <a href="#">Investors</a>
+          </p>
+          <p>
+            <a href="#">Vendors</a>
+          </p>
         </Col>
         <Col md={4} lg={2}>
           <h6>USEFUL LINKS</h6>
 
-          <p>Support</p>
-          <p>Web Player</p>
-          <p>Free Mobile App</p>
+          <p>
+            <a href="#">Support</a>
+          </p>
+          <p>
+            <a href="#">Web Player</a>
+          </p>
+          <p>
+            <a href="#">Free Mobile App</a>
+          </p>
         </Col>
-        <Col md={4} lg={4} className="text-end">
-          <h6>I T F</h6>
+        <Col md={4} lg={4} className="social-icons-container text-end">
+          <button>
+            <FontAwesomeIcon icon={faInstagram} />
+          </button>
+
+          <button>
+            <FontAwesomeIcon icon={faTwitter} />
+          </button>
+
+          <button>
+            <FontAwesomeIcon icon={faFacebookF} />
+          </button>
         </Col>
       </Row>
       <Row>
-        <Col xs={10}>
+        <Col xs={10} className=" d-flex align-items-end">
           <ul className="bottom-bar">
-            <li>Legal</li>
-            <li>lirivacy Center</li>
-            <li>lirivacy liolicy</li>
-            <li>Cookies</li>
-            <li>About Ads</li>
+            <li>
+              <a href="#">Legal</a>
+            </li>
+            <li>
+              <a href="#">lirivacy Center</a>
+            </li>
+            <li>
+              <a href="#">lirivacy liolicy</a>
+            </li>
+            <li>
+              <a href="#">Cookies</a>
+            </li>
+            <li>
+              <a href="#">About Ads</a>
+            </li>
           </ul>
+        </Col>
+        <Col className="bottom-bar text-end">
+          <p>
+            <a style={{ cursor: "pointer" }}>
+              <FontAwesomeIcon
+                icon={faGlobeAmericas}
+                style={{ marginRight: "6px" }}
+              />
+              United Kingdom
+            </a>
+          </p>
+          <p style={{ fontSize: "0.65rem" }}>© 2022 Spotify AB</p>
         </Col>
       </Row>
     </Container>
