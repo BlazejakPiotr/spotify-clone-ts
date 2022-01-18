44228075c06d1dc081ba720d8117327b6445a79d
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-01-18 02:30:29 +0100

    top nav

--- src/components/player/PlayerApp.tsx
+++ src/components/player/PlayerApp.tsx
@@ -2,22 +2,23 @@ import { Col, Container, Row } from "react-bootstrap";
 import PlayerSidebar from "./PlayerSidebar";
 import "./player.css";
 import PlayerBar from "./PlayerBar";
+import PlayerTopNav from "./PlayerTopNav";
 
 const PlayerApp = () => {
   return (
-    <Container fluid>
-      <Row className="player-container d-flex">
-        <Col className="sidebar-wrapper">
-          <PlayerSidebar />
-        </Col>
-        <Col>content</Col>
-      </Row>
-      <Row>
-        <Col className="p-0">
-          <PlayerBar />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container fluid>
+        <Row className="player-container d-flex">
+          <Col className="sidebar-wrapper">
+            <PlayerSidebar />
+          </Col>
+          <Col className="p-0">
+            <PlayerTopNav />
+          </Col>
+        </Row>
+      </Container>
+      <PlayerBar />
+    </>
   );
 };
 
---
+++ src/components/player/PlayerTopNav.tsx
@@ -0,0 +1,50 @@
+import {
+  faChevronCircleLeft,
+  faChevronCircleRight,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Dropdown, DropdownButton, Navbar } from "react-bootstrap";
+
+const PlayerTopNav = () => {
+  return (
+    <Navbar sticky="top">
+      <Container fluid>
+        <div className="top-left-nav">
+          <button>
+            <span>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </span>
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+        <div className="top-right-nav d-flex align-items-center">
+          <button>PREMIUM</button>
+          <Dropdown align={{ lg: "end" }}>
+            <Dropdown.Toggle id="user-dropdown-btn">
+              <img
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                alt="profile-pic"
+              />
+              hglock
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+              <Dropdown.Item eventKey="3" active>
+                Orange
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default PlayerTopNav;
